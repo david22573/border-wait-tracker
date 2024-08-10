716804f3a5a8b97cd50ec65de43c8c5276b401fb
David Rivero <davidmiguel22573@gmail.com>
2024-08-09 21:42:32 -0700

    progress on loading data

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
 	"os"
 	"strings"
 
@@ -9,8 +11,8 @@ import (
 )
 
 type Entry struct {
-	name string
-	body []byte
+	Name string
+	Body string
 }
 
 func main() {
@@ -23,13 +25,15 @@ func main() {
 			feedWriter(url)
 		}
 	}()
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*")
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"title": "CBP Wait Times",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+	router.GET("/info", info)
+	router.Run() // listen and serve on 0.0.0.0:8080
 }
 
 func feedWriter(url string) error {
@@ -38,9 +42,57 @@ func feedWriter(url string) error {
 	feed, _ := fp.ParseURL(url)
 	info := strings.Split(feed.Items[0].Description, "<br/>")[3]
 	entry := &Entry{
-		name: feed.Items[0].Title,
-		body: []byte(info),
+		Name: feed.Items[0].Title,
+		Body: info,
 	}
-	filename := strings.Split(feed.Items[0].Title, " ")[0]
-	return os.WriteFile(filename, entry.body, 0600)
+	cleanName, _ := cleanFilename(entry.Name)
+	filename := cleanName + ".txt"
+	return os.WriteFile(filename, []byte(entry.Body), 0600)
+}
+
+func cleanFilename(name string) (string, error) {
+	if name == "" {
+		return "", fmt.Errorf("filename cannot be empty")
+	}
+	lower := strings.ToLower(name)
+	r := strings.ReplaceAll(lower, " - ", " ")
+	cleanName := strings.ReplaceAll(r, " ", "-")
+	return cleanName, nil
+}
+
+func info(c *gin.Context) {
+	entries, err := getEntries()
+	if err != nil {
+		c.String(http.StatusInternalServerError, "Error: %v", err)
+		return
+	}
+
+	c.HTML(http.StatusOK, "info.html", gin.H{
+		"entries": entries,
+	})
+}
+
+func getEntries() ([]Entry, error) {
+	entries := []Entry{}
+	files, err := os.ReadDir(".")
+	if err != nil {
+		return entries, fmt.Errorf("error reading directory: %v", err)
+	}
+	for _, file := range files {
+		if strings.HasSuffix(file.Name(), ".txt") {
+			entry, err := loadFile(file.Name())
+			if err != nil {
+				return entries, fmt.Errorf("error reading file: %v", err)
+			}
+			entries = append(entries, Entry{
+				Name: file.Name(),
+				Body: string(entry),
+			})
+		}
+	}
+	return entries, nil
+}
+
+func loadFile(filename string) ([]byte, error) {
+	return os.ReadFile(filename)
 }
