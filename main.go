ad68befd25884b137038d31299d4fe8fbfa341a5
David Rivero <davidmiguel22573@gmail.com>
2024-08-09 17:50:53 -0700

    add file write logic

--- main.go
+++ main.go
@@ -1,13 +1,27 @@
 package main
 
 import (
+	"os"
+	"strings"
+
 	"github.com/gin-gonic/gin"
 	"github.com/mmcdole/gofeed"
 )
 
+type Entry struct {
+	name string
+	body []byte
+}
+
 func main() {
+	URLS := []string{
+		"https://bwt.cbp.gov/api/bwtRss/rssbyportnum/HTML/POV/250601",
+		"https://bwt.cbp.gov/api/bwtRss/rssbyportnum/HTML/POV/250401",
+	}
 	go func() {
-		feedWriter()
+		for _, url := range URLS {
+			feedWriter(url)
+		}
 	}()
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
@@ -18,9 +32,15 @@ func main() {
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
 
-func feedWriter() {
+func feedWriter(url string) error {
 	// Parse request
 	fp := gofeed.NewParser()
-	feed, _ := fp.ParseURL("https://www.bleepingcomputer.com/feed/")
-	println(feed.Title)
+	feed, _ := fp.ParseURL(url)
+	info := strings.Split(feed.Items[0].Description, "<br/>")[3]
+	entry := &Entry{
+		name: feed.Items[0].Title,
+		body: []byte(info),
+	}
+	filename := strings.Split(feed.Items[0].Title, " ")[0]
+	return os.WriteFile(filename, entry.body, 0600)
 }
